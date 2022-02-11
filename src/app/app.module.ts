bebf8cf9806018d9e5dfe849f772ddaa1a2c44ff
husokanus <husokanus@gmail.com>
2022-02-11 21:32:00 +0300

    bootstrap, jquery, alertifyjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import { AlertifyService } from './../../services/alertify.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor(
+    private alertifyService :AlertifyService
+  ) { }
+
+  ngOnInit(): void {
+    this.alertifyService.success("İlk denememiz başarılı olsun inş.");
+  }
+
+}
