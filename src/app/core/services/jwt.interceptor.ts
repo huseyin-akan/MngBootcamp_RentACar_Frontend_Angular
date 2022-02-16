31821c322cd613252d3ef91609dc24f9efa9bbac
husokanus <husokanus@gmail.com>
2022-02-16 17:27:16 +0300

    jwt-interceptor, admin-module, admin-guard

---
+++ src/app/core/services/jwt.interceptor.ts
@@ -0,0 +1,26 @@
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // İsteklerde ara girip eğer kullanıcı logged in ise isteğe header ekler.
+        const user = this.authService.userValue;
+        const isLoggedIn = user && user.token;
+        const isApiUrl = request.url.startsWith(environment.apiUrl);
+        if (isLoggedIn && isApiUrl) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${user.token}`
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
